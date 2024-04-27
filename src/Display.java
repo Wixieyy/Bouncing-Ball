9f08c0ba6a0656dfa02f26ee6d448f895f0e13ff
Wixieyy <shendrickwilliams@gmail.com>
2024-04-27 11:53:10 +0200

    Variable lateral movement and side wall collisions

--- src/Display.java
+++ src/Display.java
@@ -33,37 +33,37 @@ class Panel extends JPanel implements ActionListener, KeyListener {
     int coordinateY = 100;
     int gravity = 1;
     int frames = 0;
-    double dampening = 1;
+    double collisionDamping = 1;
     long startTime = System.currentTimeMillis();
-    JLabel fpsLabel, dampeningLabel;
+    JLabel fpsLabel, collisionDampingLabel;
 
     Panel() {
-        dampeningLabel = new JLabel("dampening");
+        collisionDampingLabel = new JLabel("collisionDamping");
         fpsLabel = new JLabel("fps");
         this.setPreferredSize(new Dimension(600, 600));
         this.setBackground(new Color(0xD3D3D3));
         this.addKeyListener(this);
         this.setFocusable(true);
         circle = new ImageIcon("circle.png").getImage();
-        timer = new Timer(0,this);
+        timer = new Timer(0, this);
         timer.start();
-        this.add(dampeningLabel);
+        this.add(collisionDampingLabel);
         this.add(fpsLabel);
     }
 
-    public void paint (Graphics g) {
+    public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2D = (Graphics2D) g;
         g2D.setStroke(new BasicStroke(3));
-        g2D.drawRect(50,50,500,484);
-        g2D.fillRect(50,50,500,484);
+        g2D.drawRect(50, 50, 500, 484);
+        g2D.fillRect(50, 50, 500, 484);
         g2D.drawImage(circle, coordinateX, coordinateY, null);
     }
 
     private double fpsCounter() {
         long currentTime = System.currentTimeMillis();
         long elapsedTime = currentTime - startTime;
-        double fps = 0;
+        double fps;
         fps = (double) frames / (elapsedTime / 1000.0);
         fps = Math.round(fps * 10.0) / 10.0;
         frames = 0;
@@ -74,16 +74,24 @@ class Panel extends JPanel implements ActionListener, KeyListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         int imageHeight = circle.getHeight(null) / 2;
+        int imageWidth = circle.getWidth(null) / 2;
+
         if (coordinateY + imageHeight >= PANEL_HEIGHT) {
-            gravity = (int) (Math.abs(gravity) * -1 * dampening);
+            gravity = (int) (Math.abs(gravity + velocityY) * -1 * collisionDamping);
         } else {
             gravity++;
         }
         coordinateY = coordinateY + gravity;
+
+        if (coordinateX <= 45 || coordinateX + imageWidth >= PANEL_WIDTH + 25) {
+            velocityX = velocityX * -1;
+        }
+        coordinateX = coordinateX + velocityX;
+
         frames++;
         if (frames % 10 == 0) {
-            fpsLabel.setText("FPS: " + String.valueOf(fpsCounter()));
-            dampeningLabel.setText("Dampening: " + String.format("%.1f",dampening));
+            fpsLabel.setText("FPS: " + fpsCounter());
+            collisionDampingLabel.setText("Damping force: " + String.format("%.2f", collisionDamping));
         }
         repaint();
     }
@@ -97,14 +105,25 @@ class Panel extends JPanel implements ActionListener, KeyListener {
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-            case KeyEvent.VK_E:
-                dampening = Math.min(dampening + 0.1, 1);
+            case KeyEvent.VK_D:
+                collisionDamping = Math.min(collisionDamping + 0.01, 1);
                 break;
-            case KeyEvent.VK_Q:
-                dampening = Math.max(dampening - 0.1, 0);
+            case KeyEvent.VK_A:
+                collisionDamping = Math.max(collisionDamping - 0.01, 0);
                 break;
-            case KeyEvent.VK_R:
-
+            case KeyEvent.VK_W:
+                if (velocityX < 0) {
+                    velocityX = velocityX - 1;
+                } else {
+                    velocityX = velocityX + 1;
+                }
+                break;
+            case KeyEvent.VK_S:
+                if (velocityX < 0) {
+                    velocityX = velocityX + 1;
+                } else {
+                    velocityX = velocityX - 1;
+                }
                 break;
         }
     }
